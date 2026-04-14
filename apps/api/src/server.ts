f2a7fab66680e3ad8ce3d954cd4ceebcc9294ade
Alisson <alissonboucinhas.dev@gmail.com>
2026-04-14 16:00:06 -0300

    start repo

---
+++ apps/api/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+	console.log(req.url);
+	res.send("Hello World!");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="border-y-indigo-50">
+			<h1 className="text-emerald-500">Hello, World!</h1>
+		</div>
+	);
+}
+
+export default App;
